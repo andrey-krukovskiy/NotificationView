d1801d3ad2623d08bc436f43798e78331c947c29
Andrey Krukovskiy <andrey.krukovskiy@zo.email>
2020-01-27 11:47:19 +0300

    Hide bottom view by another way

--- NotificationView/Classes/NotificationBannerView.swift
+++ NotificationView/Classes/NotificationBannerView.swift
@@ -97,6 +97,7 @@ class NotificationBannerView: UIView {
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor(white: 215/255, alpha: 1)
         view.layer.cornerRadius = 1.5
+        view.isHidden = true
         return view
     }()
     
@@ -160,7 +161,7 @@ class NotificationBannerView: UIView {
         self.addSubview(self.containerView)
         self.containerView.addSubview(self.headerView)
         self.containerView.addSubview(self.textContainerView)
-//        self.containerView.addSubview(self.bottomView)
+        self.containerView.addSubview(self.bottomView)
         
         self.headerView.addSubview(self.iconImageView)
         self.headerView.addSubview(self.appNameLabel)
@@ -197,16 +198,16 @@ class NotificationBannerView: UIView {
         
         // bottom
         
-//        self.containerView.addConstraints([
-//            NSLayoutConstraint(item: self.textContainerView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomView, attribute: .top, multiplier: 1, constant: -8).priority(900),
-//            NSLayoutConstraint(item: self.containerView, attribute: .centerX, relatedBy: .equal, toItem: self.bottomView, attribute: .centerX, multiplier: 1, constant: 0),
-//            NSLayoutConstraint(item: self.containerView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomView, attribute: .bottom, multiplier: 1, constant: 5).priority(900)
-//        ])
+        self.containerView.addConstraints([
+            NSLayoutConstraint(item: self.textContainerView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomView, attribute: .top, multiplier: 1, constant: -8).priority(900),
+            NSLayoutConstraint(item: self.containerView, attribute: .centerX, relatedBy: .equal, toItem: self.bottomView, attribute: .centerX, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: self.containerView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomView, attribute: .bottom, multiplier: 1, constant: 5).priority(900)
+        ])
         
-//        self.bottomView.addConstraints([
-//            NSLayoutConstraint(item: self.bottomView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 35),
-//            NSLayoutConstraint(item: self.bottomView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 3.5).priority(900)
-//        ])
+        self.bottomView.addConstraints([
+            NSLayoutConstraint(item: self.bottomView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 35),
+            NSLayoutConstraint(item: self.bottomView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 3.5).priority(900)
+        ])
         
         // icon
         
